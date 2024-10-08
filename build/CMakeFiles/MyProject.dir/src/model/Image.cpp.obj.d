6433d22f344b23ca3d51e5045407770b206f4ece
JungHun Yun <19jyun@korea.ac.kr>
2024-10-08 16:27:07 +0900

    Made changes from drawable to shape pointer

---
+++ build/CMakeFiles/MyProject.dir/src/model/Image.cpp.obj.d
@@ -0,0 +1,3 @@
+CMakeFiles/MyProject.dir/src/model/Image.cpp.obj: \
+ C:\personal_proj\cpp_oop\src\model\Image.cpp \
+ C:\personal_proj\cpp_oop\src\model\Image.h
---
+++ build/CMakeFiles/MyProject.dir/src/view/ShapeView.cpp.obj.d
@@ -0,0 +1,3 @@
+CMakeFiles/MyProject.dir/src/view/ShapeView.cpp.obj: \
+ C:\personal_proj\cpp_oop\src\view\ShapeView.cpp \
+ C:\personal_proj\cpp_oop\src\view\ShapeView.h
