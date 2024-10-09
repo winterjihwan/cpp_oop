79277b4d2d293636ec743a48591ee7b51c39bca1
Ji Hwan <angeljihwan@gmail.com>
2024-10-09 09:32:03 +0900

    fix: remove build dir from git

--- build/CMakeFiles/MyProject.dir/src/model/Canvas.cpp.obj.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/MyProject.dir/src/model/Canvas.cpp.obj: \
- C:\personal_proj\cpp_oop\src\model\Canvas.cpp \
- C:\personal_proj\cpp_oop\src\model\Canvas.h
--- build/CMakeFiles/MyProject.dir/src/model/Image.cpp.obj.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/MyProject.dir/src/model/Image.cpp.obj: \
- C:\personal_proj\cpp_oop\src\model\Image.cpp \
- C:\personal_proj\cpp_oop\src\model\Image.h
